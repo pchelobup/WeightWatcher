e81932ed7a2f63acf22ed8022a1e1c3ff222322e
Pchelobup <Alinadavydova0710@gmail.com>
2022-03-24 12:58:50 +0700

    add log

--- src/main/java/ru/alina/service/NotationService.java
+++ src/main/java/ru/alina/service/NotationService.java
@@ -1,5 +1,7 @@
 package ru.alina.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alina.model.Notation;
@@ -10,6 +12,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class NotationService {
+    private final Logger log = LoggerFactory.getLogger(NotationService.class);
     private final NotationRepository weightRepository;
 
     public NotationService(NotationRepository weightRepository) {
@@ -18,19 +21,27 @@ public class NotationService {
 
     @Transactional
     public Notation save(Notation notation, int userId){
+        log.info("save {} for user {}", notation, userId);
         return weightRepository.save(notation, userId);
     }
 
     @Transactional
     public void delete(int id, int userId){
+        log.info("delete notation {} for user {}", id, userId);
         weightRepository.delete(id, userId);
     }
 
     public Notation get(int id, int userId) {
+        log.info("get notation {} for user {}", id, userId);
         return weightRepository.get(id, userId);
     }
 
     public List<Notation> getAll(int userId) {
+        log.info("getAll notation for user {}", userId);
         return weightRepository.getAll(userId);
     }
 }
+
+
+
+
--- src/main/java/ru/alina/service/UserService.java
+++ src/main/java/ru/alina/service/UserService.java
@@ -1,5 +1,7 @@
 package ru.alina.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.alina.model.User;
@@ -11,6 +13,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 public class UserService {
+    private final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -19,19 +22,23 @@ public class UserService {
 
     @Transactional
     public User save(User user) {
+        log.info("save user {}", user);
         return userRepository.save(user);
     }
 
     @Transactional
     public void delete(int id){
+        log.info("delete user {}", id);
         userRepository.delete(id);
     }
 
     public User get(int id) {
+        log.info("get user {}", id);
         return userRepository.get(id);
     }
 
     public List<User> getAll(){
+        log.info("getAll user");
         return userRepository.getAll();
     }
 }
