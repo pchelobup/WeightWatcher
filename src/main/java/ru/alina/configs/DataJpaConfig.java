031e5abe4fb561ce44792f73f90e4c77bc7d3f65
Pchelobup <Alinadavydova0710@gmail.com>
2022-03-24 16:23:16 +0700

    add jpa properties

--- src/main/java/ru/alina/configs/DataJpaConfig.java
+++ src/main/java/ru/alina/configs/DataJpaConfig.java
@@ -1,8 +1,12 @@
 package ru.alina.configs;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
@@ -12,19 +16,47 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "ru.alina.repository")
-@Profile("hsql")
-public class DataJpaHsqlConfig {
+@PropertySource("classpath:db.properties")
+public class DataJpaConfig {
+    @Autowired
+    private Environment env;
+
 
     @Bean
+    @Profile("default")
+    public DataSource dataSourcePostgres() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setUrl(env.getProperty("db.url"));
+        dataSource.setUsername( env.getProperty("db.username") );
+        dataSource.setPassword(env.getProperty("db.password"));
+        return dataSource;
+    }
+
+
+
+    @Bean
+    @Profile("default")
+    public JpaVendorAdapter jpaVendorAdapterPostgres() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.POSTGRESQL);
+        adapter.setGenerateDdl(true);
+        return adapter;
+    }
+
+    @Bean
+    @Profile("hsql")
     public DataSource dataSourceHSQL() {
         EmbeddedDatabaseBuilder dataSource = new EmbeddedDatabaseBuilder();
         return dataSource.setType(EmbeddedDatabaseType.HSQL).addScript("classpath:db/initHSQL.sql").build();
     }
 
     @Bean
+    @Profile("hsql")
     public JpaVendorAdapter jpaVendorAdapterHsql() {
         HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
         adapter.setDatabase(Database.HSQL);
@@ -32,12 +64,21 @@ public class DataJpaHsqlConfig {
         return adapter;
     }
 
+
+
     @Bean("entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean localContainerEMF(DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        Properties jpaProperties = new Properties();
+        jpaProperties.put("hibernate.show_sql", true);
+        jpaProperties.put("hibernate.format_sql", true);
+        jpaProperties.put("hibernate.use_sql_comments", true);
+
         LocalContainerEntityManagerFactoryBean emf = new LocalContainerEntityManagerFactoryBean();
         emf.setDataSource(dataSource);
         emf.setJpaVendorAdapter(jpaVendorAdapter);
         emf.setPackagesToScan("ru.alina.model");// JPA entity classes will be loaded from this package
+        emf.setJpaProperties(jpaProperties);
+
         return emf;
     }
 }
--- src/test/java/ru/alina/service/ServiceTest.java
+++ src/test/java/ru/alina/service/ServiceTest.java
@@ -6,11 +6,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import ru.alina.configs.DataJpaPostgresConfig;
+import ru.alina.configs.DataJpaConfig;
 import ru.alina.configs.RootConfig;
 
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ContextConfiguration(classes = {DataJpaPostgresConfig.class, RootConfig.class})
+@ContextConfiguration(classes = {DataJpaConfig.class, RootConfig.class})
 @ActiveProfiles("hsql")
 @ExtendWith(SpringExtension.class)
 public abstract class ServiceTest {
