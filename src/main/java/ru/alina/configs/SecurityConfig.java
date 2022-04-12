b477f0c64ff28507d967223f4fd43258f2aaa9f5
Pchelobup <Alinadavydova0710@gmail.com>
2022-04-13 01:59:14 +0700

    auto login after registration

--- src/main/java/ru/alina/configs/SecurityConfig.java
+++ src/main/java/ru/alina/configs/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -64,5 +66,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         return daoAuthenticationProvider;
+    }
+
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/ru/alina/web/AuthController.java
+++ src/main/java/ru/alina/web/AuthController.java
@@ -1,7 +1,12 @@
 package ru.alina.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,16 +14,23 @@ import org.springframework.web.context.request.WebRequest;
 import ru.alina.model.Role;
 import ru.alina.model.User;
 import ru.alina.service.UserService;
-
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 
 @Controller
 public class AuthController {
     private UserService userService;
 
+    protected AuthenticationManager authenticationManager;
+
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
+    }
+
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
     }
 
     @GetMapping("/login")
@@ -32,10 +44,10 @@ public class AuthController {
     }
 
     @PostMapping("/registration")
-    public String SingIn(WebRequest request) {
-        String email = request.getParameter("email");
-        String password = request.getParameter("password");
-        String passwordConfirm = request.getParameter("password-confirm");
+    public String SingIn(HttpServletRequest httpServletRequest) {
+        String email = httpServletRequest.getParameter("email");
+        String password = httpServletRequest.getParameter("password");
+        String passwordConfirm = httpServletRequest.getParameter("password-confirm");
         //TODO test on null
         if (!password.equals(passwordConfirm) || userService.isLoginExist(email)) {
             //TODO оповестить пользователя
@@ -48,6 +60,21 @@ public class AuthController {
         user.setRoles(Collections.singleton(Role.USER));
         //TODO оповестить и записать в лог если юзер null
         userService.save(user);
-        return "singIn";
+        authWithAuthManager(httpServletRequest, email, password);
+        return "redirect:/";
+    }
+
+    public void authWithAuthManager(HttpServletRequest request, String username, String password) {
+        try{
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+            authToken.setDetails(new WebAuthenticationDetails(request));
+            Authentication authentication = authenticationManager.authenticate(authToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        catch (Exception e){
+            e.getStackTrace();
+        }
+
+
     }
 }
